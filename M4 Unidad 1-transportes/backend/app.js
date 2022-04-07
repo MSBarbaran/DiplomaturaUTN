9dd96f9720bc99b5de09bb7cbff2091daea111f5
María Sol <sol.barbaran@gmail.com>
2022-04-07 12:43:45 -0300

    M5 U4-Sitio de Transportes-Login Logout y Rutas privadas integrando Node.js con B.D.

---
+++ M4 Unidad 1-transportes/backend/app.js
@@ -0,0 +1,80 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// Agregamos el siguiente requiere para enlazar el archivo .env: 
+require('dotenv').config();
+// Configuramos la variable de session
+var session = require('express-session');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+// primera ruta loginRoute
+var loginRouter = require('./routes/admin/login');
+// segunda ruta novedades
+var adminRouter = require('./routes/admin/novedades');
+const { cookie } = require('express/lib/response');
+const async = require('hbs/lib/async');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// complemento la variable de session
+app.use(session({
+  secret: '1234',
+  cookie: { maxAge: null },
+  resave: false,
+  saveUninitialized: true
+}))
+// funcion de seguridad
+secured = async (req, res, next) => {
+  try {
+    console.log(req.session.id_usuario);
+    if (req.session.id_usuario){
+      next();
+    } else {
+      res.redirect('/admin/login');
+    }// cierre del else
+  } catch (error) {
+    console.log(error);
+  } // cierre de catch error
+} // cierre de secured
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+// complemento de la ruta login
+app.use('/admin/login', loginRouter);
+// complemento de la ruta novedades y secured
+app.use('/admin/novedades',secured, adminRouter);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
