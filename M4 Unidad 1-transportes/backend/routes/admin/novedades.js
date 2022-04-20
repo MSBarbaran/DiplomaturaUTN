692311a53978a1706482f0c8718b745628e0b168
María Sol <sol.barbaran@gmail.com>
2022-04-20 20:20:39 -0300

    M6 U2- CRUD (parte 2) - sitio de trasportes

--- M4 Unidad 1-transportes/backend/routes/admin/novedades.js
+++ M4 Unidad 1-transportes/backend/routes/admin/novedades.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { route } = require('express/lib/application');
 const async = require('hbs/lib/async');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel');
@@ -15,13 +16,12 @@ router.get('/', async function(req, res, next) {
    });
 });
 
-// para agregar una novedad
+// --- para agregar una novedad---
 router.get('/agregar', (req,res,next) =>{
   res.render('admin/agregar', {
     layout: 'admin/layout'
   }) //cierra render
 }); //cierra Get
-
 
 router.post('/agregar', async (req,res,next) => {
   try{
@@ -45,5 +45,45 @@ router.post('/agregar', async (req,res,next) => {
 // si los 3 datos fueron completados (titulo, subtitulo y cuerpo) inserto una novedad y redireccion a la ruta admin/novedades
 // si falta por completar algun campo aparece un error y el siguiente mensaje 'Todos los campos son requeridos'
 // si no se conecta al servidor aparece el siguiente error y mensage 'No se cargo la novedad'
+
+//--- para eliminar una novedad ---
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades')
+}); //cierra el get para eliminar
+
+//elimino por id, me conecto con novedades borro y me devuelve a la pagina asignada
+
+//--- para modificar una novedad ---
+//traer la  novedad por id para modidicar
+router.get('/modificar/:id', async (req, res, next) => {
+
+  let id = req.params.id;
+  let novedad = await novedadesModel.getNovedadById(id);
+  res.render('admin/modificar', {
+    layout:'admin/layout',
+    novedad
+  });
+});
+//modificar update
+router.post('/modificar', async (req, res, next) => {
+  try{
+    let obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+   }
+   catch (error) {
+     console.log(error)
+     res.render('admin/modificar', {
+       layout: 'admin/layout',
+       error: true, message: 'No se pudo modificar la novedad'
+     })
+   }
+})
 
 module.exports = router;
