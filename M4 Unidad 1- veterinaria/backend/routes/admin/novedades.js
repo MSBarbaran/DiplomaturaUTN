c0d02aeb124bb10efe29e801d01270bc5ca4514f
María Sol <sol.barbaran@gmail.com>
2022-04-21 11:51:01 -0300

    M6 U2- CRUD (parte 2) - sitio veterinaria

--- M4 Unidad 1- veterinaria/backend/routes/admin/novedades.js
+++ M4 Unidad 1- veterinaria/backend/routes/admin/novedades.js
@@ -46,4 +46,44 @@ router.post('/agregar', async (req,res,next) => {
 // si falta por completar algun campo aparece un error y el siguiente mensaje 'Todos los campos son requeridos'
 // si no se conecta al servidor aparece el siguiente error y mensage 'No se cargo la novedad'
 
+//--- para eliminar una novedad ---
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades')
+}); //cierra el get para eliminar
+
+//elimino por id, me conecto con novedades borro y me devuelve a la pagina asignada
+
+//--- para modificar una novedad ---
+//traer la  novedad por id para modidicar
+router.get('/modificar/:id', async (req, res, next) => {
+
+  let id = req.params.id;
+  let novedad = await novedadesModel.getNovedadById(id);
+  res.render('admin/modificar', {
+    layout:'admin/layout',
+    novedad
+  });
+});
+//modificar update
+router.post('/modificar', async (req, res, next) => {
+  try{
+    let obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+   }
+   catch (error) {
+     console.log(error)
+     res.render('admin/modificar', {
+       layout: 'admin/layout',
+       error: true, message: 'No se pudo modificar la novedad'
+     })
+   }
+})
+
 module.exports = router;
