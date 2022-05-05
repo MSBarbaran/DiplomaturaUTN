004d17f963be6bd487a625155a4029385607983a
María Sol <sol.barbaran@gmail.com>
2022-05-05 18:18:47 -0300

    M6-U4- Envio de formulario - sitio veterinaria

--- M4 Unidad 1- veterinaria/backend/routes/api.js
+++ M4 Unidad 1- veterinaria/backend/routes/api.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
 router.get('/novedades', async function (req, res, next) {
     let novedades = await novedadesModel.getNovedades();
 
     novedades = novedades.map(novedades => {
+        novedades.cuerpo = novedades.cuerpo.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '<br/>'+ '$2');
         if (novedades.img_id) {
             const imagen = cloudinary.url(novedades.img_id, {
                 width: 260,
@@ -27,5 +29,30 @@ router.get('/novedades', async function (req, res, next) {
 
     res.json(novedades);
 });
+
+router.post('/contacto', async (req, res) => {
+    const mail = {
+        to: 'sol.barbaran@gmail.com',
+        subject: 'Contacto web',
+        html: `${req.body.nombre} se contactó a través de la web y quiere más información a este correo: ${req.body.email} <br> Además, hizo el siguiente comentario: ${req.body.mensaje} <br> y su teléfono es: ${req.body.telefono}`
+    }
+
+    const transport = nodemailer.createTransport ({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje enviado con exito'
+    });
+});
+
 
 module.exports = router;
--- M4 Unidad 1- veterinaria/frontend/src/pages/ContactoPage.js
+++ M4 Unidad 1- veterinaria/frontend/src/pages/ContactoPage.js
@@ -1,8 +1,44 @@
 import '../styles/components/pages/ContactoPage.css'
 
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
+// enviar el formulario
 const ContactoPage = (props) => {
+
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true)
+        const response = await axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm)
+        }
+    }
+
+
+    // formulario
     return (
         <main className="holder">
             <div className="nosotros">
@@ -18,24 +54,24 @@ const ContactoPage = (props) => {
                         <li><i className="fab fa-instagram"></i> <b>Instagram</b> veterinariax </li>
                     </ul>
                 </div>
-                <div className="contacto-form">
+                <div className="contacto-form" >
                     <h4>Contactanos</h4>
-                    <form action="" method="" className="formulario">
+                    <form action="/contacto" method="post" className="formulario" onSubmit={handleSubmit}>
                         <p>
                             <label>Nombre</label>
-                            <input type="text" name="nombre" />
+                            <input type="text" name="nombre" value={formData.nombre} onChange={handleChange}/>
                         </p>
                         <p>
                             <label>Email</label>
-                            <input type="text" name="email" />
+                            <input type="text" name="email" value={formData.email} onChange={handleChange} />
                         </p>
                         <p>
                             <label>Teléfono</label>
-                            <input type="text" name="telefono" />
+                            <input type="text" name="telefono" value={formData.telefono} onChange={handleChange} />
                         </p>
                         <p>
                             <label>Comentario</label>
-                            <textarea name="mensaje"></textarea>
+                            <textarea name="mensaje" value={formData.mensaje} onChange={handleChange}></textarea>
                         </p>
                         <p>
                         <button className="centrar"type="submit" value="Enviar">
@@ -43,6 +79,8 @@ const ContactoPage = (props) => {
                         </button>
                          </p>
                     </form>
+                    {sending ? <p className='alertaUno'><i className="fas fa-circle-notch fa-spin"></i> <span> Enviando...</span></p>: null}
+                    {msg ? <p className='alertaDos'><i className='fas fa-check'> <span> {msg} </span></i></p> : null}
                 </div>
                 
             </div>
